d2677bb2459a3225f15cb3fa09fed8a898c6d6a2
Nicolas Holanda <nikolas.nhm14@gmail.com>
2025-07-18 07:51:41 -0300

    Initial commit

---
+++ src/test/java/com/github/nicolasholanda/spring_camel/SpringCamelApplicationTests.java
@@ -0,0 +1,13 @@
+package com.github.nicolasholanda.spring_camel;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringCamelApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
